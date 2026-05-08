b2d1eb86f4b6772037736a037a45954d81471d40
lamdt4 <lamdt@scsolutions.vn>
2026-05-08 09:31:18 +0700

    Task 5: add functional role model

--- src/models/tenant/TenantInterface.ts
+++ src/models/tenant/TenantInterface.ts
@@ -1,17 +1,25 @@
 import type { ISODateString } from "../common";
 
-export type AuthRole = 'tenant_admin' | 'agent'
+export enum FunctionalPathEnum {
+  MANAGER = 'manager',
+  DISTRIBUTOR = 'distributor',
+  AFFILIATE = 'affiliatesystem',
+  POS = 'pos',
+  COLLABORATOR = 'collaborator'
+}
+
+export type AuthRole = FunctionalPathEnum
 
 export interface ITenantItem {
-    id: string | number
-    name: string
-    logoUrl?: string
-    planName?: string
-    ClientId?: string
-    ClientSecret?: string
-    isActive?: boolean
-    createdAt?: ISODateString
-    expiresAt?: ISODateString
+  id: string | number
+  name: string
+  logoUrl?: string
+  planName?: string
+  ClientId?: string
+  ClientSecret?: string
+  isActive?: boolean
+  createdAt?: ISODateString
+  expiresAt?: ISODateString
 }
 
 export interface IAgentItem {
