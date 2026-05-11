a852735e5d723914ccd88d802d3fdee0bf6bd4d7
lamdt4 <lamdt@scsolutions.vn>
2026-05-11 18:16:39 +0700

    continue to fix

--- src/services/access/accessService.ts
+++ src/services/access/accessService.ts
@@ -4,9 +4,9 @@ export class AuthService {
   static login = authService.login
   static refresh = authService.refresh
   static logout = authService.logout
-  static logoutAll = authService.logoutAll
-  static getMe = authService.getMe
-  static updateMe = authService.updateMe
+  // static logoutAll = authService.logoutAll
+  // static getMe = authService.getMe
+  // static updateMe = authService.updateMe
   static getSessions = authService.getSessions
   static revokeSession = authService.revokeSession
   static getStoredTokens = authService.getStoredTokens
